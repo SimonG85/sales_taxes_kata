9729acfdad6566828a1eb35e98ddbd18b8ab550b
Simon Garruto <simon.garruto@gmail.com>
2025-01-02 16:40:16 +0100

    refactor: added data to enum

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,9 @@ enum Imported {
 }
 
 enum Category {
-    Book,
-    Food,
-    Medical,
+    Book(String),
+    Food(String),
+    Medical(String),
     Other(String),
 }
 
@@ -36,6 +36,22 @@ impl Item {
     }
 }
 
+impl ToString for Item {
+    fn to_string(&self) -> String {
+        let name = match &self.category {
+            Category::Book(x) | Category::Food(x) | Category::Medical(x) | Category::Other(x) => x,
+        };
+
+        let prefix = if matches!(self.imported, Imported::Yes) {
+            "1 imported "
+        } else {
+            "1 "
+        };
+
+        format!("{}{}: {}", prefix, name, self.get_prices().0)
+    }
+}
+
 fn round_numbers(number: f64) -> f64 {
     (number * 20.0).round() / 20.0
 }
@@ -43,13 +59,13 @@ fn round_numbers(number: f64) -> f64 {
 impl Tax for Item {
     fn get_prices(&self) -> (f64, f64) {
         match (&self.category, &self.imported) {
-            (Category::Book | Category::Food | Category::Medical, Imported::No) => {
+            (Category::Book(_) | Category::Food(_) | Category::Medical(_), Imported::No) => {
                 (self.clean_price, 0.0)
             }
             (Category::Other(_), Imported::No) => {
                 (self.clean_price, round_numbers(self.clean_price * 0.10))
             }
-            (Category::Book | Category::Food | Category::Medical, Imported::Yes) => {
+            (Category::Book(_) | Category::Food(_) | Category::Medical(_), Imported::Yes) => {
                 (self.clean_price, round_numbers(self.clean_price * (0.05)))
             }
             (Category::Other(_), Imported::Yes) => (
@@ -70,7 +86,7 @@ mod tests {
     use approx::assert_relative_eq;
     #[test]
     fn test_book() {
-        let book = Item::new(12.49, Imported::No, Category::Book).unwrap();
+        let book = Item::new(12.49, Imported::No, Category::Book("book".to_string())).unwrap();
         let (clean_price, tax) = book.get_prices();
         let expected = (12.49, 0.0);
         assert_relative_eq!(clean_price, expected.0, epsilon = f64::EPSILON);
@@ -87,7 +103,8 @@ mod tests {
     }
     #[test]
     fn test_imported_box_chocolates() {
-        let box_chocolates = Item::new(10.00, Imported::Yes, Category::Food).unwrap();
+        let box_chocolates =
+            Item::new(10.00, Imported::Yes, Category::Food("".to_string())).unwrap();
         let (clean_price, tax) = box_chocolates.get_prices();
         let expected = (10.0, 0.50);
         assert_relative_eq!(clean_price, expected.0, epsilon = f64::EPSILON);
@@ -114,11 +131,11 @@ mod multiple_item_tests {
     use approx::assert_relative_eq;
     #[test]
     fn test_purchase_1() {
-        let book = Item::new(12.49, Imported::No, Category::Book).unwrap();
+        let book = Item::new(12.49, Imported::No, Category::Book("".to_string())).unwrap();
         let book_prices = book.get_prices();
         let music_cd = Item::new(14.99, Imported::No, Category::Other("CD".to_string())).unwrap();
         let music_cd_prices = music_cd.get_prices();
-        let bar_chocolates = Item::new(0.85, Imported::No, Category::Food).unwrap();
+        let bar_chocolates = Item::new(0.85, Imported::No, Category::Food("".to_string())).unwrap();
         let bar_chocolates_prices = bar_chocolates.get_prices();
         let clean_price = book_prices.0 + music_cd_prices.0 + bar_chocolates_prices.0;
         let taxes = book_prices.1 + music_cd_prices.1 + bar_chocolates_prices.1;
@@ -127,7 +144,8 @@ mod multiple_item_tests {
     }
     #[test]
     fn test_purchase_2() {
-        let chocolates_box = Item::new(10.00, Imported::Yes, Category::Food).unwrap();
+        let chocolates_box =
+            Item::new(10.00, Imported::Yes, Category::Food("".to_string())).unwrap();
         let choc_box_prices = chocolates_box.get_prices();
         let imported_perfume = Item::new(
             47.50,
@@ -157,9 +175,11 @@ mod multiple_item_tests {
         )
         .unwrap();
         let perf_prices = perfume.get_prices();
-        let headache_pills = Item::new(9.75, Imported::No, Category::Medical).unwrap();
+        let headache_pills =
+            Item::new(9.75, Imported::No, Category::Medical("".to_string())).unwrap();
         let pills_prices = headache_pills.get_prices();
-        let imported_chocolates = Item::new(11.25, Imported::Yes, Category::Food).unwrap();
+        let imported_chocolates =
+            Item::new(11.25, Imported::Yes, Category::Food("".to_string())).unwrap();
         let imported_choc_prices = imported_chocolates.get_prices();
 
         let clean_price =
@@ -176,15 +196,15 @@ mod item_to_string_tests {
     use super::*;
     #[test]
     fn test_book() {
-        let book = Item::new(12.49, Imported::No, Category::Book).unwrap();
-        let book_to_string = "1 book at 12.49".to_string();
+        let book = Item::new(12.49, Imported::No, Category::Book("book".to_string())).unwrap();
+        let book_to_string = "1 book: 12.49".to_string();
         assert_eq!(book.to_string(), book_to_string);
     }
     #[test]
     fn test_music_cd() {
         let music_cd =
             Item::new(16.49, Imported::No, Category::Other("music CD".to_string())).unwrap();
-        let music_cd_to_string = "1 music CD at 16.49".to_string();
+        let music_cd_to_string = "1 music CD: 16.49".to_string();
         assert_eq!(music_cd.to_string(), music_cd_to_string);
     }
 }
